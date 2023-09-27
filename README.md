30dc86b9132e0da0c837c3e456b2644b31c97137
tamzi <tamrefrank@gmail.com>
2023-09-27 15:46:40 +0200

    have fun

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# AndroidCodingChallenge
+# Android Coding Challenge
+
+We appreciate you taking the time to participate and submit a coding challenge. 
+In the next step we would like you to implement a list of image dispaly feature with unit and/or integration tests
+that fetches data from [The SpaceX API](https://github.com/r-spacex/SpaceX-API/blob/master/docs/README.md) 
+
+Below you will find a list of tasks and limitations
+required for completing the challenge.
+
+
+### Application:
+
+* Users can see space x detailed launches information
+* Users can see description of the launches.
+* Users can bookmark any launches and view them later.(Don't worry about sending the information to the cloud, saving it locally is good)
+
+### Important:
+
+* test accordingly
+* use Jetpack Compose
+* Documentation of technical decisions you make during the challenge. 
+* The API implementation and the architecture of the code is completely up to you.
+
+
+### What if I don't finish?
+
+Try to produce something that is at least minimally functional. Part of the exercise is to see what you prioritize first
+when you have a limited amount of time. For any unfinished tasks, please add comments to your code with a
+short explanation. You will be given an opportunity later to go into more detail and explain how you would go about
+finishing those tasks.
+
+
+Have fun! that is the most important thing!
+Looking forward to your submission!
---
+++ app/src/main/java/com/thermondo/androidchallenge/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.thermondo.androidchallenge
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.thermondo.androidchallenge.ui.theme.AndroidCodingChallengeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AndroidCodingChallengeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("thermondo Android coding challenge")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AndroidCodingChallengeTheme {
+        Greeting("thermondo Android coding challenge")
+    }
+}
